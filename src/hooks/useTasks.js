a8b1f78c4d26632d06f39b9c7f137b09f28bf5ce
joseagl90 <gl.josea90@gmail.com>
2025-07-12 14:11:58 +0200

    agregando cap y fix

--- src/hooks/useTasks.js
+++ src/hooks/useTasks.js
@@ -1,10 +1,41 @@
 import { useLocalStorage } from "./useLocalStorage";
+import React from "react";
 
 export function useTasks() {
   const [tasks, setTasks] = useLocalStorage("tasks", []);
 
+  const cleanDuplicateIds = (taskList) => {
+    const seen = new Set();
+    return taskList.map(task => {
+      if (seen.has(task.id)) {
+        const newId = task.id + Math.random();
+        seen.add(newId);
+        return { ...task, id: newId };
+      } else {
+        seen.add(task.id);
+        return task;
+      }
+    });
+  };
+
+  React.useEffect(() => {
+    const ids = new Set();
+    let hasDuplicate = false;
+    for (const task of tasks) {
+      if (ids.has(task.id)) {
+        hasDuplicate = true;
+        break;
+      }
+      ids.add(task.id);
+    }
+    if (hasDuplicate) {
+      const cleanedTasks = cleanDuplicateIds(tasks);
+      setTasks(cleanedTasks);
+    }
+  }, []);
+
   const addTask = (title, dateId) => {
-    const id = dateId !== undefined ? dateId : Date.now();
+    const id = dateId !== undefined ? dateId + Math.random() : Date.now();
     setTasks([...tasks, { id, title, done: false }]);
   };
 
